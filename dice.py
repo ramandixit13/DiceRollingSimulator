1b735c5a36fc3bea4f3fd60f1e0fd4213cc3b64a
Deepanshu5 <72217307+Deepanshu5@users.noreply.github.com>
2020-10-02 11:37:31 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,8 +1,6 @@
 import random
-#When you run the program you'll get a random number between 1-6
-roll_again="yes"
-#if you want to roll again you can give the input accordingly
-while(roll_again=="yes" or roll_again=="y"):
-    #random.randint() function will give you a random integer from the specified range
-    print(random.randint(1,7))
-    roll_again=input("Do you want to roll again: y/yes or n/no:: ")
+a=input("Yes for roll again, No for not roll again")
+if a=="yes":
+    dice_number=random.randint(1,6)
+
+print(dice_number)
